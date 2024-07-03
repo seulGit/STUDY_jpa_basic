831eac3b32bc688d1cdd9727dccfcbb83203a225
seulGit <seulgi9421@gmail.com>
2024-07-03 22:31:40 +0900

    240703 - 양방향 연관관계와 연관관계의 주인

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +12,10 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+    // add 할 때 npe 안 뜨도록 관례로 많이 씀
 
     public Long getId() {
         return id;
@@ -27,5 +31,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
